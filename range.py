d4fa79552f093fd0815eb29dde986829cfc693f9
Tchalz <chibuzor_jonathan@yahoo.com>
2020-02-22 15:49:23 -0800

    second commit

---
+++ range.py
@@ -0,0 +1,30 @@
+# def sort_number(number):
+# result = int(number) * 100
+# print(result)
+# sort_number(10)
+
+"""
+def sort_number(a, b, c):
+    result = int(a) * int(b) * int(c)
+    print(result)
+
+#the function below is called outside of the function and not inside of the function else it will not operate.
+ sort_number(2, 4, 5)
+"""
+
+"""
+def sort_number():
+    a = input("a:")
+    b = input("b:")
+    c = input("c:")
+    result = int(a) * int(b) * int(c)
+    print(result)
+
+
+# the function below is called outside of the function not inside of the function
+sort_number()
+"""
+
+x = " emmanuel_is _getting_so_real"
+print(len(x))
+print(x{3})
